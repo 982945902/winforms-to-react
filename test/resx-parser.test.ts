1bb8a1ce3fa9fc803a86808bc4a7414a6019a896
982945902 <982945902@qq.com>
2026-06-30 18:30:26 +0800

    Add resx parser test (19/19 pass)
    Test verifies: Location/Size/Text/Anchor/ClientSize/Font/Enabled extraction,
    >>metadata entries skipped,  form properties parsed correctly.

---
+++ test/resx-parser.test.ts
@@ -0,0 +1,81 @@
+import { mkdtemp, writeFile, rm } from "node:fs/promises";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+import { describe, expect, it } from "vitest";
+import { parseResx, applyResxToProps } from "../src/parser/resxParser.js";
+
+const SAMPLE_RESX = `<?xml version="1.0" encoding="utf-8"?>
+<root>
+  <resheader name="resmimetype"><value>text/microsoft-resx</value></resheader>
+  <data name="$this.ClientSize" type="System.Drawing.Size, System.Drawing">
+    <value>400, 300</value>
+  </data>
+  <data name="$this.Text" xml:space="preserve">
+    <value>My Form</value>
+  </data>
+  <data name="btnOK.Location" type="System.Drawing.Point, System.Drawing">
+    <value>12, 264</value>
+  </data>
+  <data name="btnOK.Size" type="System.Drawing.Size, System.Drawing">
+    <value>75, 23</value>
+  </data>
+  <data name="btnOK.Text" xml:space="preserve">
+    <value>OK</value>
+  </data>
+  <data name="btnOK.Anchor" type="System.Windows.Forms.AnchorStyles, System.Windows.Forms">
+    <value>Bottom, Right</value>
+  </data>
+  <data name="lblName.Location" type="System.Drawing.Point, System.Drawing">
+    <value>12, 12</value>
+  </data>
+  <data name="lblName.Text" xml:space="preserve">
+    <value>Name:</value>
+  </data>
+  <data name="lvItems.Font" type="System.Drawing.Font, System.Drawing">
+    <value>Microsoft Sans Serif, 9.75pt</value>
+  </data>
+  <data name="lvItems.Enabled" type="System.Boolean, mscorlib">
+    <value>False</value>
+  </data>
+  <data name=">>btnOK.Name" xml:space="preserve">
+    <value>btnOK</value>
+  </data>
+</root>`;
+
+describe("resxParser", () => {
+  it("parses control properties from resx XML", async () => {
+    const dir = await mkdtemp(join(tmpdir(), "resx-test-"));
+    const resxPath = join(dir, "TestForm.resx");
+    try {
+      await writeFile(resxPath, SAMPLE_RESX, "utf8");
+      const data = await parseResx(resxPath);
+
+      // btnOK properties
+      const btnProps = applyResxToProps("btnOK", data);
+      expect(btnProps.location).toEqual({ x: 12, y: 264 });
+      expect(btnProps.size).toEqual({ width: 75, height: 23 });
+      expect(btnProps.text).toBe("OK");
+      expect(btnProps.anchor).toEqual(["Bottom", "Right"]);
+
+      // Label text
+      const lblProps = applyResxToProps("lblName", data);
+      expect(lblProps.text).toBe("Name:");
+      expect(lblProps.location).toEqual({ x: 12, y: 12 });
+
+      // Form properties ($this)
+      const formProps = applyResxToProps("$this", data);
+      expect(formProps.clientSize).toEqual({ width: 400, height: 300 });
+      expect(formProps.text).toBe("My Form");
+
+      // Font and Enabled
+      const lvProps = applyResxToProps("lvItems", data);
+      expect(lvProps.font).toEqual({ family: "Microsoft Sans Serif", size: 9.75 });
+      expect(lvProps.enabled).toBe(false);
+
+      // >> entries (designer metadata) should be skipped
+      expect(data.has(">>btnOK.Name")).toBe(false);
+    } finally {
+      await rm(dir, { recursive: true, force: true });
+    }
+  });
+});
